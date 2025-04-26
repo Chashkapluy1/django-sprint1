285eefddf9d055c86eacce514f2ad4391c51dd16
Pavel lagerev <pavellagerev@yandex.ru>
2025-04-26 22:17:05 +0300

    commt

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,31 +1,31 @@
 """
-URL configuration for blogicum project.
+Настройка URL-адресов для проекта blogicum.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+Список `urlpatterns` направляет URL-адреса к представлениям.
+Документация Django по URL-адресам:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
-
-Examples:
-    Function views:
-        1. Add an import: from my_app import views
-        2. Add a URL: path('', views.home, name='home')
-    
-    Class-based views:
-        1. Add an import: from other_app.views import Home
-        2. Add a URL: path('', Home.as_view(), name='home')
-    
-    Including another URLconf:
-        1. Import include(): from django.urls import include, path
-        2. Add a URL: path('blog/', include('blog.urls'))
+Примеры:
+    1. Для обычных представлений (function views):
+        - Импортируйте views: from my_app import views
+        - Добавьте URL: path('', views.home, name='home')
+    2. Для класс-базированных представлений:
+        - Импортируйте класс: from other_app.views import Home
+        - Добавьте URL: path('', Home.as_view(), name='home')
+    3. Для включения других URL-конфигураций:
+        - Импортируйте include: from django.urls import include, path
+        - Добавьте URL: path('blog/', include('blog.urls'))
 """
 from django.urls import include, path
 
-
+# Пространство имен для URL всего проекта
 app_name = 'blogicum'
 
+# Основные URL-шаблоны проекта
 urlpatterns = [
-    # Blog app routes
+    # Подключение URL-адресов приложения blog
+    # Базовый URL: / (корень сайта)
     path('', include('blog.urls', namespace='blog')),
-    
-    # Pages app routes
+    # Подключение URL-адресов приложения pages
+    # Базовый URL: /pages/
     path('pages/', include('pages.urls', namespace='pages')),
 ]
