6624d2c3b393b29b2d69047b57fe95f7190b5c2b
Pavel lagerev <pavellagerev@yandex.ru>
2025-04-27 06:46:40 +0300

    Ревью1

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 from . import views
 
-
+# Имя приложения
 app_name = 'blog'
 
+# Список URL-адресов
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('posts/<int:post_id>/', 
-        views.post_detail, 
-        name='post_detail'),
+    path('', views.index, name='index'),  # Главная страница
+    path('posts/<int:post_id>/',
+         views.post_detail,
+         name='post_detail'),  # Страница с деталями поста
     path('category/<slug:category_slug>/',
-        views.category_posts,
-        name='category_posts'),
+         views.category_posts,
+         name='category_posts'),  # Страница с постами по категории
 ]
